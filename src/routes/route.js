5bef9e390bbaae9cc8631dc8bee5b018ed69f3a9
KANISHKMAKKAR <kanishk.makkar.333@gmail.com>
2022-05-23 15:53:22 +0530

    AWS_FRIST DAY

--- src/routes/route.js
+++ src/routes/route.js
@@ -17,11 +17,21 @@ router.get("/test-me", function (req, res) {
 // -you can never use await on callback..if you awaited something , then you can be sure it is within a promise
 // -how to write promise:- wrap your entire code inside: "return new Promise( function(resolve, reject) { "...and when error - return reject( err )..else when all ok and you have data, return resolve (data)
 
+// aws.config.update({
+//     accessKeyId: "AKIAY3L35MCRVFM24Q7U",
+//     secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
+//     region: "ap-south-1"
+// })
+// aws.config.update({
+//     accessKeyId: "AKIAY3L35MCRVFM24Q7U",
+//     secretAccessKey: "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",
+//     region: "ap-south-1"
+// })
 aws.config.update({
-    accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-    secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
+accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
+    secretAccessKey: "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",
     region: "ap-south-1"
-})
+     })
 
 let uploadFile= async ( file) =>{
    return new Promise( function(resolve, reject) {
@@ -31,7 +41,7 @@ let uploadFile= async ( file) =>{
     var uploadParams= {
         ACL: "public-read",
         Bucket: "classroom-training-bucket",  //HERE
-        Key: "abc/" + file.originalname, //HERE 
+        Key: "kanishk/" + file.originalname, //HERE 
         Body: file.buffer
     }
 
